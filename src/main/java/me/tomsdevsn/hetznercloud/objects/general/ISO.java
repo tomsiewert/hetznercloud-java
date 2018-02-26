00d79009a82ccbd42cbc8f25e978fbf97368a28a
Tom Siewert <tom@tomsdevsn.me>
2018-02-26 20:59:06 +0100

    Added deprecated timestamp to ISO

--- src/main/java/me/tomsdevsn/hetznercloud/objects/general/ISO.java
+++ src/main/java/me/tomsdevsn/hetznercloud/objects/general/ISO.java
@@ -1,6 +1,11 @@
 package me.tomsdevsn.hetznercloud.objects.general;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
+import me.tomsdevsn.hetznercloud.deserialize.DateDeserializer;
+import me.tomsdevsn.hetznercloud.objects.response.Meta;
+
+import java.util.Date;
 
 @Data
 public class ISO {
@@ -9,5 +14,8 @@ public class ISO {
     private String name;
     private String description;
     private String type;
+    @JsonDeserialize(using = DateDeserializer.class)
+    private Date deprecated;
+    private Meta meta;
 
 }
