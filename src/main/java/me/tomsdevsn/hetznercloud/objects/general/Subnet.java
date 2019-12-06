58ea4712df777c06f8d6a2f113c60086e8fac6de
Christian Pelster <pelle@pelle.io>
2019-12-06 22:50:28 +0100

    add builder for subnet and fix typo

--- src/main/java/me/tomsdevsn/hetznercloud/objects/general/Subnet.java
+++ src/main/java/me/tomsdevsn/hetznercloud/objects/general/Subnet.java
@@ -1,15 +1,21 @@
 package me.tomsdevsn.hetznercloud.objects.general;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Subnet {
 
     private String type;
     @JsonProperty("ip_range")
     private String ipRange;
-    @JsonProperty("neetwork_zone")
+    @JsonProperty("network_zone")
     private String networkZone;
     private String gateway;
 }
