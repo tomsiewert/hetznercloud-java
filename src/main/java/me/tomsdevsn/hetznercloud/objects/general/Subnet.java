7eb48395e4ead17d240f53fa2772526c95ca2b5c
Tom Michael Siewert <tom@siewert.io>
2020-01-13 13:57:49 +0100

    Update Subnet.java

--- src/main/java/me/tomsdevsn/hetznercloud/objects/general/Subnet.java
+++ src/main/java/me/tomsdevsn/hetznercloud/objects/general/Subnet.java
@@ -1,15 +1,9 @@
 package me.tomsdevsn.hetznercloud.objects.general;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class Subnet {
 
     private String type;
