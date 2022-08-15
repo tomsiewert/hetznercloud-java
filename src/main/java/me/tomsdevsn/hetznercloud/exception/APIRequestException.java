c2b4cbca6961f542c7f73e7791700f370bd828db
Felix Krasemann <hi@felix-krasemann.de>
2022-08-15 23:14:28 +0200

    added serialVersionUID + standard constructors

--- src/main/java/me/tomsdevsn/hetznercloud/exception/APIRequestException.java
+++ src/main/java/me/tomsdevsn/hetznercloud/exception/APIRequestException.java
@@ -5,11 +5,31 @@ import me.tomsdevsn.hetznercloud.objects.response.APIErrorResponse;
 
 public class APIRequestException extends RuntimeException {
 
-    @Getter
-    private final APIErrorResponse APIErrorResponse;
+    private static final long serialVersionUID = -5504832422225211786L;
+    private static final String DEFAULT_EXCEPTION_MSG = "Encountered an Error while calling the Hetzner-API: [%s] %s";
 
-    public APIRequestException(APIErrorResponse APIErrorResponse) {
-        this.APIErrorResponse = APIErrorResponse;
+    @Getter
+    private final APIErrorResponse apiErrorResponse;
+
+    public APIRequestException(APIErrorResponse apiErrorResponse) {
+        this(String.format(DEFAULT_EXCEPTION_MSG,
+                apiErrorResponse.getError().getCode(),
+                apiErrorResponse.getError().getMessage()), null, apiErrorResponse);
+    }
+
+    public APIRequestException(String message, APIErrorResponse apiErrorResponse) {
+        this(message, null, apiErrorResponse);
+    }
+
+    public APIRequestException(Throwable cause, APIErrorResponse apiErrorResponse) {
+        this(cause != null ? cause.getMessage() : null, cause, apiErrorResponse);
+    }
+
+    public APIRequestException(String message, Throwable cause, APIErrorResponse apiErrorResponse) {
+        super(message);
+        if (cause != null) super.initCause(cause);
+
+        this.apiErrorResponse = apiErrorResponse;
     }
 
 }
