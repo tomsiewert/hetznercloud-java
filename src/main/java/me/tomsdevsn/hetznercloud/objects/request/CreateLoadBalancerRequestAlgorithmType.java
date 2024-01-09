fe431c92997d62863d2a555744b680a829b90bc9
easyStartup-pulse <132751110+easyStartup-pulse@users.noreply.github.com>
2024-01-09 08:32:21 +0530

    Load balancer Algorithm fix
    https://docs.hetzner.cloud/#load-balancers-create-a-load-balancer

---
+++ src/main/java/me/tomsdevsn/hetznercloud/objects/request/CreateLoadBalancerRequestAlgorithmType.java
@@ -0,0 +1,18 @@
+package me.tomsdevsn.hetznercloud.objects.request;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CreateLoadBalancerRequestAlgorithmType {
+
+    @JsonProperty(value = "type")
+    private String type;
+
+}
